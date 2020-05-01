4660cfe60771e0198b434c5c91706d91aa7299ce
KimSooHwan <sh951011@gmail.com>
2020-05-02 00:57:15 +0900

    Fix Naming

--- attention.py
+++ attention.py
@@ -4,9 +4,6 @@
         author={Soohwan Kim},
         year={2020}
     }
-
-    Pytorch Implementation of some attention
-    any questions, bug reports or recommends, please Contacts sh951011@gmail.com
 """
 
 import torch
@@ -14,83 +11,88 @@ import torch.nn as nn
 import torch.functional as F
 
 
+# Pytorch Implementation of some attention
+# any questions, bug reports or recommends, please Contacts sh951011@gmail.com
+
+
 class MultiHeadAttention(nn.Module):
-    def __init__(self, in_features, n_head=4, dim=128):
+
+    def __init__(self, in_features, num_head=8, dim=64):
         super(MultiHeadAttention, self).__init__()
         self.in_features = in_features
-        self.n_head = n_head
+        self.num_head = num_head
         self.dim = dim
-        self.W = nn.Linear(in_features, dim * n_head)
-        self.V = nn.Linear(in_features, dim * n_head)
-        self.fc = nn.Linear(in_features << 1, in_features)
+        self.W_Q = nn.Linear(in_features, dim * num_head)
+        self.W_V = nn.Linear(in_features, dim * num_head)
+        self.fc = nn.Linear(in_features + dim * num_head, in_features)
 
-    def forward(self, queries, values):
-        batch_size = values.size(0)
-        query_length = queries.size(1)
-        value_length = values.size(1)
+    def forward(self, Q, V):
+        batch_size = V.size(0)
+        residual = Q
 
-        preserved = queries
+        q_s = self.W_Q(Q).view(batch_size, -1, self.num_head, self.dim)
+        v_s = self.W_V(V).view(batch_size, -1, self.num_head, self.dim)
 
-        queries = self.W(queries).view(batch_size, query_length, self.n_head, self.dim)
-        values = self.V(values).view(batch_size, value_length, self.n_head, self.dim)
+        q_s = q_s.permute(2, 0, 1, 3).contiguous().view(batch_size * self.num_head, -1, self.dim)
+        v_s = v_s.permute(2, 0, 1, 3).contiguous().view(batch_size * self.num_head, -1, self.dim)
 
-        queries = queries.permute(2, 0, 1, 3).contiguous().view(-1, query_length, self.dim)
-        values = values.permute(2, 0, 1, 3).contiguous().view(-1, value_length, self.dim)
+        attn_score = torch.bmm(q_s, v_s.transpose(1, 2))
+        align = F.softmax(attn_score, dim=2)
 
-        attn_score = torch.bmm(queries, values.transpose(1, 2))
-        alignment = F.softmax(attn_score, dim=2)
+        attn_val = torch.bmm(align, v_s).view(self.num_head, batch_size, -1, self.dim)
+        attn_val = attn_val.permute(1, 2, 0, 3).contiguous().view(batch_size, -1, self.num_head * self.dim)
+        combined = torch.cat([attn_val, residual], dim=2)
 
-        attn_val = torch.bmm(alignment, values).view(self.n_head, batch_size, query_length, self.dim)
-        attn_val = attn_val.permute(1, 2, 0, 3).contiguous().view(batch_size, query_length, -1)
-
-        combined = torch.cat([attn_val, preserved], dim=2)
-        context = torch.tanh(self.fc(combined.view(-1, 2 * self.in_features))).view(batch_size, -1, self.in_features)
+        context = torch.tanh(self.fc(combined.view(-1, self.in_features + self.dim * self.num_head)))
+        context = context.view(batch_size, -1, self.in_features)
 
         return context
 
 
 class AdditiveAttention(nn.Module):
+
     def __init__(self, hidden_dim):
         super().__init__()
-        self.V = nn.Linear(hidden_dim, hidden_dim)
-        self.W = nn.Linear(hidden_dim, hidden_dim)
+        self.W_V = nn.Linear(hidden_dim, hidden_dim)
+        self.W_Q = nn.Linear(hidden_dim, hidden_dim)
         self.fc = nn.Linear(hidden_dim, 1)
 
-    def forward(self, queries, values, inputs):
-        queries = queries.transpose(0, 1)
+    def forward(self, Q, V, inputs):
+        Q = Q.transpose(0, 1)
 
-        attn_score = self.fc(torch.tanh(self.V(values) + self.W(queries))).squeeze(-1)
+        attn_score = self.fc(torch.tanh(self.W_V(V) + self.W_Q(Q))).squeeze(-1)
         align = F.softmax(attn_score, -1)
-        context = torch.bmm(align.unsqueeze(1), values)
+        context = torch.bmm(align.unsqueeze(1), V)
 
         return context
 
 
-class HybridAttention(nn.Module):
-    def __init__(self, dec_hidden_dim, enc_hidden_dim, context_dim, k=10, smoothing=True):
-        super(HybridAttention, self).__init__()
+class LocationAwareAttention(nn.Module):
+
+    def __init__(self, dec_hidden_dim, enc_hidden_dim, attn_dim, k=10, smoothing=True):
+        super(LocationAwareAttention, self).__init__()
         self.dec_hidden_dim = dec_hidden_dim
-        self.context_dim = context_dim
+        self.attn_dim = attn_dim
         self.smoothing = smoothing
-        self.loc_conv = nn.Conv1d(in_channels=1, out_channels=k, kernel_size=3, padding=1)
-        self.W = nn.Linear(dec_hidden_dim, context_dim, bias=False)
-        self.V = nn.Linear(enc_hidden_dim, context_dim, bias=False)
-        self.U = nn.Linear(k, context_dim, bias=False)
-        self.b = nn.Parameter(torch.FloatTensor(context_dim).uniform_(-0.1, 0.1))
-        self.fc = nn.Linear(context_dim, 1, bias=False)
+        self.conv = nn.Conv1d(in_channels=1, out_channels=k, kernel_size=3, padding=1)
+        self.W_Q = nn.Linear(dec_hidden_dim, attn_dim, bias=False)
+        self.W_V = nn.Linear(enc_hidden_dim, attn_dim, bias=False)
+        self.W_U = nn.Linear(k, attn_dim, bias=False)
+        self.b = nn.Parameter(torch.FloatTensor(attn_dim).uniform_(-0.1, 0.1))
+        self.fc = nn.Linear(attn_dim, 1, bias=False)
         self.tanh = nn.Tanh()
         self.softmax = nn.Softmax(dim=-1)
 
-    def forward(self, queries, values, last_align):
-        batch_size = queries.size(0)
-        hidden_dim = queries.size(2)
+    def forward(self, Q, V, last_align):
+        batch_size = Q.size(0)
+        hidden_dim = Q.size(2)
 
-        conv_attn = torch.transpose(self.loc_conv(last_align.unsqueeze(1)), 1, 2)
-        attn_score = self.w(
+        U = torch.transpose(self.conv(last_align.unsqueeze(1)), 1, 2)
+        attn_score = self.fc(
             self.tanh(
-                self.W(queries.reshape(-1, hidden_dim)).view(batch_size, -1, self.context_size)
-                + self.V(values.reshape(-1, hidden_dim)).view(batch_size, -1, self.context_size)
-                + self.U(conv_attn)
+                self.W_Q(Q.reshape(-1, hidden_dim)).view(batch_size, -1, self.attn_dim)
+                + self.W_V(V.reshape(-1, hidden_dim)).view(batch_size, -1, self.attn_dim)
+                + self.W_U(U)
                 + self.b
             )
         ).squeeze(dim=-1)
@@ -103,34 +105,35 @@ class HybridAttention(nn.Module):
             align = self.softmax(attn_score)
 
         context = torch.bmm(align.unsqueeze(dim=1), values).squeeze(dim=1)
-
         return context, align
 
 
 class ContentBasedAttention(nn.Module):
-    def __init__(self, dec_hidden_dim, enc_hidden_dim, context_dim):
+
+    def __init__(self, dec_hidden_dim, enc_hidden_dim, attn_dim):
         super(ContentBasedAttention, self).__init__()
-        self.W = nn.Linear(dec_hidden_dim, context_dim, bias=False)
-        self.V = nn.Linear(enc_hidden_dim, context_dim, bias=False)
-        self.b = nn.Parameter(torch.FloatTensor(context_dim).uniform_(-0.1, 0.1))
-        self.fc = nn.Linear(context_dim, 1, bias=False)
-        self.context_dim = context_dim
+        self.W_Q = nn.Linear(dec_hidden_dim, attn_dim, bias=False)
+        self.W_V = nn.Linear(enc_hidden_dim, attn_dim, bias=False)
+        self.b = nn.Parameter(torch.FloatTensor(attn_dim).uniform_(-0.1, 0.1))
+        self.fc = nn.Linear(attn_dim, 1, bias=False)
+        self.attn_dim = attn_dim
         self.tanh = nn.Tanh()
         self.softmax = nn.Softmax(dim=-1)
 
-    def forward(self, decoder_output, encoder_outputs):
-        batch_size = decoder_output.size(0)
-        hidden_size = decoder_output.size(2)
+    def forward(self, Q, V):
+        batch_size = Q.size(0)
+        hidden_dim = Q.size(2)
 
         attn_score = self.fc(
             self.tanh(
-                self.W(decoder_output.reshape(-1, hidden_size)).view(batch_size, -1, self.context_dim)
-                + self.V(encoder_outputs.reshape(-1, hidden_size)).view(batch_size, -1, self.context_dim)
+                self.W_Q(Q.reshape(-1, hidden_dim)).view(batch_size, -1, self.attn_dim)
+                + self.W_V(V.reshape(-1, hidden_dim)).view(batch_size, -1, self.attn_dim)
                 + self.b
             )
         ).squeeze(dim=-1)
+
         align = self.softmax(attn_score)
-        context = torch.bmm(align.unsqueeze(dim=1), encoder_outputs).squeeze(dim=1)
+        context = torch.bmm(align.unsqueeze(dim=1), V).squeeze(dim=1)
 
         return context
 
@@ -140,16 +143,16 @@ class DotProductAttention(nn.Module):
         super(DotProductAttention, self).__init__()
         self.fc = nn.Linear(dec_hidden_dim * 2, dec_hidden_dim)
 
-    def forward(self, queries, values):
-        batch_size = queries.size(0)
-        hidden_dim = queries.size(2)
-        input_size = values.size(1)
+    def forward(self, Q, V):
+        batch_size = Q.size(0)
+        hidden_dim = Q.size(2)
+        input_size = V.size(1)
 
-        attn_score = torch.bmm(queries, values.transpose(1, 2))
+        attn_score = torch.bmm(Q, V.transpose(1, 2))
         align = F.softmax(attn_score.view(-1, input_size), dim=1).view(batch_size, -1, input_size)
-        context = torch.bmm(align, values)
+        attn_val = torch.bmm(align, V)
 
-        combined = torch.cat((context, queries), dim=2)
+        combined = torch.cat((attn_val, Q), dim=2)
         context = torch.tanh(self.fc(combined.view(-1, 2 * hidden_dim))).view(batch_size, -1, hidden_dim)
 
         return context
