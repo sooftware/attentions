5fbf11c323d6572d6c44de2a476a1040f7b81966
KimSooHwan <sh951011@gmail.com>
2020-05-26 23:32:14 +0900

    Add MultiHeadedLocationAware Attention

--- attention.py
+++ attention.py
@@ -1,9 +1,9 @@
 """
-    @source_code{
-        title={some attention implementation},
-        author={Soohwan Kim},
-        year={2020}
-    }
+@source_code{
+    title={some attention implementation},
+    author={Soohwan Kim},
+    year={2020}
+}
 """
 
 import torch
@@ -12,6 +12,116 @@ import torch.nn as nn
 
 # Pytorch Implementation of some attention
 # any questions, bug reports or recommends, please Contacts sh951011@gmail.com
+
+class MultiHeadedLocationAwareAttention(nn.Module):
+    r"""
+    Multi-headed Location-Aware Attention
+    Applies a multi-head + location-aware attention mechanism on the output features from the decoder.
+    Multi-head attention proposed in "Attention Is All You Need" paper.
+    Location-aware attention proposed in "Attention-Based Models for Speech Recognition" paper.
+    We combined these two attention mechanisms as custom.
+
+    Args:
+        in_features (int): The number of expected features in the output
+        num_heads (int): The number of heads. (default: )
+        conv_out_channel (int): The number of out channel in convolution
+
+    Inputs: query, value, prev_align
+        - **query** (batch, q_len, hidden_dim): tensor containing the output features from the decoder.
+        - **value** (batch, v_len, hidden_dim): tensor containing features of the encoded input sequence.
+        - **prev_align** (batch_size * num_heads, v_len): tensor containing previous timestep`s alignment
+
+    Returns: output, align
+        - **output** (batch, output_len, dimensions): tensor containing the attended output features from the decoder.
+        - **align** (batch * num_heads, v_len): tensor containing the alignment from the encoder outputs.
+
+    Reference:
+        - **Attention Is All You Need**: https://arxiv.org/abs/1706.03762
+        - **Attention-Based Models for Speech Recognition**: https://arxiv.org/abs/1506.07503
+        - **State-Of-The-Art Speech Recognition with Sequence-to-Sequence Models**: https://arxiv.org/abs/1712.01769
+    """
+    def __init__(self, in_features, num_heads=8, conv_out_channel=10):
+        super().__init__()
+        self.in_features = in_features
+        self.num_heads = num_heads
+        self.dim = int(in_features / num_heads)
+        self.conv1d = nn.Conv1d(num_heads, conv_out_channel, kernel_size=3, padding=1)
+        self.loc_proj = nn.Linear(conv_out_channel, self.dim, bias=False)
+        self.q_proj = nn.Linear(in_features, self.dim * num_heads, bias=False)
+        self.v_proj = nn.Linear(in_features, self.dim * num_heads, bias=False)
+        self.bias = nn.Parameter(torch.rand(self.dim).uniform_(-0.1, 0.1))
+        self.score_proj = nn.Linear(self.dim, 1, bias=True)
+        self.out_proj = nn.Linear(in_features << 1, in_features, bias=True)
+
+    def forward(self, query, value, prev_align):  # value : BxTxD
+        batch_size, seq_len = value.size(0), value.size(1)
+
+        if prev_align is None:
+            prev_align = value.new_zeros(batch_size, self.num_heads, seq_len)  # BxNxT
+
+        score = self.get_attn_score(query, value, prev_align, batch_size, seq_len)
+        align = F.softmax(score, dim=1)  # BNxT
+
+        value = value.view(batch_size, seq_len, self.num_heads, self.dim).permute(0, 2, 1, 3)
+        value = value.contiguous().view(-1, seq_len, self.dim)  # BNxTxD
+
+        context = torch.bmm(align.unsqueeze(1), value).view(batch_size, -1, self.num_heads * self.dim)  # BNx1xT x BNxTxD => BNxD
+        align = align.view(batch_size, self.num_heads, -1)  # BNxT => BxNxT
+
+        combined = torch.cat([context, query], dim=2)
+        output = self.out_proj(combined.view(-1, self.in_features << 1)).view(batch_size, -1, self.in_features)
+
+        return output, align
+
+    def get_attn_score(self, query, value, prev_align, batch_size, seq_len):
+        loc_energy = torch.tanh(self.loc_proj(self.conv1d(prev_align).transpose(1, 2)))  # BxNxT => BxTxD
+        loc_energy = loc_energy.unsqueeze(1).repeat(1, self.num_heads, 1, 1).view(-1, seq_len, self.dim)  # BxNxTxD => BNxTxD
+
+        query = self.q_proj(query).view(batch_size, -1, self.num_heads, self.dim).permute(0, 2, 1, 3)  # BxNxTxD
+        value = self.v_proj(value).view(batch_size, -1, self.num_heads, self.dim).permute(0, 2, 1, 3)  # BxNxTxD
+        query = query.contiguous().view(-1, 1, self.dim)  # BNx1xD
+        value = value.contiguous().view(-1, seq_len, self.dim)  # BNxTxD
+
+        score = self.score_proj(torch.tanh(value + query + loc_energy + self.bias)).squeeze(2)  # BNxTxD => BNxT
+        return score
+
+
+class LocationAwareAttention(nn.Module):
+    """ Implementation of Location-Aware Attention (Hybrid Attention) """
+    def __init__(self, dec_hidden_dim, enc_hidden_dim, attn_dim, k=10, smoothing=True):
+        super(LocationAwareAttention, self).__init__()
+        self.dec_hidden_dim = dec_hidden_dim
+        self.attn_dim = attn_dim
+        self.smoothing = smoothing
+        self.conv1d = nn.Conv1d(in_channels=1, out_channels=k, kernel_size=3, padding=1)
+        self.linear_q = nn.Linear(dec_hidden_dim, attn_dim, bias=False)
+        self.linear_v = nn.Linear(enc_hidden_dim, attn_dim, bias=False)
+        self.linear_u = nn.Linear(k, attn_dim, bias=False)
+        self.bias = nn.Parameter(torch.rand(attn_dim).uniform_(-0.1, 0.1))
+        self.linear_out = nn.Linear(attn_dim, 1, bias=True)
+        self.tanh = nn.Tanh()
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, query, value, prev_align):
+        batch_size, hidden_dim = query.size(0), query.size(2)
+
+        conv_feat = torch.transpose(self.conv1d(prev_align.unsqueeze(1)), 1, 2)
+        attn_score = self.linear_out(self.tanh(
+                self.linear_q(query.reshape(-1, hidden_dim)).view(batch_size, -1, self.attn_dim)
+                + self.linear_v(value.reshape(-1, hidden_dim)).view(batch_size, -1, self.attn_dim)
+                + self.linear_u(conv_feat)
+                + self.bias
+        )).squeeze(dim=-1)
+
+        if self.smoothing:
+            attn_score = torch.sigmoid(attn_score)
+            align = torch.div(attn_score, attn_score.sum(dim=-1).unsqueeze(dim=-1))
+
+        else:
+            align = self.softmax(attn_score)
+
+        context = torch.bmm(align.unsqueeze(dim=1), value).squeeze(dim=1)
+        return context, align
 
 
 class ScaledDotProductAttention(nn.Module):
@@ -41,14 +151,14 @@ class ScaledDotProductAttention(nn.Module):
         return context, align
 
 
-class MultiHybridAttention(nn.Module):
+class CustomizingAttention(nn.Module):
     r"""
-    Multi-Head + Location-Aware (Hybrid) Attention
+    Customizing Attention
 
     Applies a multi-head + location-aware attention mechanism on the output features from the decoder.
     Multi-head attention proposed in "Attention Is All You Need" paper.
     Location-aware attention proposed in "Attention-Based Models for Speech Recognition" paper.
-    We combined these two attention mechanisms as custom.
+    I combined these two attention mechanisms as custom.
 
     Args:
         in_features (int): The number of expected features in the output
@@ -60,7 +170,7 @@ class MultiHybridAttention(nn.Module):
         - **value** (batch, v_len, hidden_dim): tensor containing features of the encoded input sequence.
         - **prev_align** (batch_size * num_heads, v_len): tensor containing previous timestep`s alignment
 
-    Returns: output
+    Returns: output, align
         - **output** (batch, output_len, dimensions): tensor containing the attended output features from the decoder.
         - **align** (batch * num_heads, v_len): tensor containing the alignment from the encoder outputs.
 
@@ -71,7 +181,7 @@ class MultiHybridAttention(nn.Module):
     """
 
     def __init__(self, in_features, num_heads=8, k=10):
-        super(MultiHybridAttention, self).__init__()
+        super(CustomizingAttention, self).__init__()
         self.in_features = in_features
         self.num_heads = num_heads
         self.dim = int(in_features / num_heads)
@@ -187,44 +297,6 @@ class AdditiveAttention(nn.Module):
         align = nn.softmax(attn_score)
         context = torch.bmm(align.unsqueeze(1), value)
         return context
-
-
-class LocationAwareAttention(nn.Module):
-    """ Implementation of Location-Aware Attention (Hybrid Attention) """
-    def __init__(self, dec_hidden_dim, enc_hidden_dim, attn_dim, k=10, smoothing=True):
-        super(LocationAwareAttention, self).__init__()
-        self.dec_hidden_dim = dec_hidden_dim
-        self.attn_dim = attn_dim
-        self.smoothing = smoothing
-        self.conv1d = nn.Conv1d(in_channels=1, out_channels=k, kernel_size=3, padding=1)
-        self.linear_q = nn.Linear(dec_hidden_dim, attn_dim, bias=False)
-        self.linear_v = nn.Linear(enc_hidden_dim, attn_dim, bias=False)
-        self.linear_u = nn.Linear(k, attn_dim, bias=False)
-        self.bias = nn.Parameter(torch.rand(attn_dim).uniform_(-0.1, 0.1))
-        self.linear_out = nn.Linear(attn_dim, 1, bias=True)
-        self.tanh = nn.Tanh()
-        self.softmax = nn.Softmax(dim=-1)
-
-    def forward(self, query, value, prev_align):
-        batch_size, hidden_dim = query.size(0), query.size(2)
-
-        conv_feat = torch.transpose(self.conv1d(prev_align.unsqueeze(1)), 1, 2)
-        attn_score = self.linear_out(self.tanh(
-                self.linear_q(query.reshape(-1, hidden_dim)).view(batch_size, -1, self.attn_dim)
-                + self.linear_v(value.reshape(-1, hidden_dim)).view(batch_size, -1, self.attn_dim)
-                + self.linear_u(conv_feat)
-                + self.bias
-        )).squeeze(dim=-1)
-
-        if self.smoothing:
-            attn_score = torch.sigmoid(attn_score)
-            align = torch.div(attn_score, attn_score.sum(dim=-1).unsqueeze(dim=-1))
-
-        else:
-            align = self.softmax(attn_score)
-
-        context = torch.bmm(align.unsqueeze(dim=1), value).squeeze(dim=1)
-        return context, align
 
 
 class ContentBasedAttention(nn.Module):
